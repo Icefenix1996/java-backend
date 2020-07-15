5555b14156eff0209b5225303bf0ce79b123837b
Viacheslav <54596101+Icefenix1996@users.noreply.github.com>
2020-07-15 21:06:55 +0300

    1 homework

---
+++ newfile (1).java
@@ -0,0 +1,12 @@
+public class Introduction {
+	public static void main(String[] args) {
+		String s = (" привет меня зовут слава ");
+for (int i = 0; i < 5; i++) {
+System.out.print(s);
+}
+	String z = (" я хочу быть разработчиком ");
+for (int i = 0; i < 5; i++) {
+System.out.println(z);
+}
+	}
+}
