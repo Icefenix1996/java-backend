f28daa8bfe018caf3bf72e5793088940523daeaa
Viacheslav <54596101+Icefenix1996@users.noreply.github.com>
2020-07-25 22:48:25 +0300

    Add files via upload

---
+++ 3 homework.java
@@ -0,0 +1,57 @@
+class ProgramTask31 {
+    public static void main(String[] args) {
+        int a = 127;
+        int b = 255;
+        byte c = (byte) (a - b);
+    }
+}
+
+class  ProgramTask32 {
+    public  static  void  main ( String [] args ) {
+int a2 = -327684;
+int b2 = 32768;
+short c2 = (short) (a2 - b2);
+
+    }
+}
+
+class  ProgramTask33 {
+    public static void main(String[] args) {
+        int x = 1222;
+        int y = 1222 * 1222;
+        int z = y * x;
+        System.out.println("x");
+        System.out.println("y");
+        System.out.println("z");
+    }
+}
+
+class  ProgramTask34 {
+    public  static  void  main ( String [] args ) {
+        System.out.println("Целый тип long имеет размер 8 байт (64 бита). Минимальное значение " +
+                "-9 223 372 036 854 775 808, максимальное значение 9 223 372 036 854 775 807");
+    }
+}
+
+public  class  ProgramTask35 {
+    public  static  void  main ( String [] args ) {
+        float a = 1245,554;
+        double b = 256,3785;
+        double z = a * b;
+        System.out.println("z");
+    }
+}
+
+class  ProgramTask36 {
+    public  static  void  main ( String [] args ) {
+       char x = 'z';
+       char y = '!';
+       char z = '?';
+    }
+}
+
+class  ProgramTask37 {
+     public static void main(String[] args) {
+         System.out.println("those that will be compiled without errors: c,l, i,h");
+     }
+ }
