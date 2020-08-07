f4f8e1a62a846d59a01ba3eb8b0b050a39b0e762
Viacheslav <54596101+Icefenix1996@users.noreply.github.com>
2020-08-07 09:23:15 +0300

    Update ProgramTask33.java

--- homework 3/src/ProgramTask33.java
+++ homework 3/src/ProgramTask33.java
@@ -2,7 +2,7 @@ public class ProgramTask33 {
     public static void main(String[] args) {
         int x = 1222;
         int y = x * x;
-        int z = y * x;
+        int z = x * x * x;
         System.out.println("x");
         System.out.println("y");
         System.out.println("z");
