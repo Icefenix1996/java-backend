df3e5c13954a638c42e8e05d9f90d6418dccd6fa
Viacheslav <54596101+Icefenix1996@users.noreply.github.com>
2020-08-07 09:19:22 +0300

    Update 1 homework.java

--- 1 homework.java
+++ 1 homework.java
@@ -1,7 +1,7 @@
 public class Introduction {
 
     public static void main(String[] args) {
-        String s = (" привет меня зовут слава ");
+        String s = " привет меня зовут слава ";
         for (int i = 0; i < 5; i++) {
             System.out.print(s);
         }
